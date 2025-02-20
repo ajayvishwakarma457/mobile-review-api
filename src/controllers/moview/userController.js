2e6e57dbd5594964016cb57361861297aa87256d
Ajay Spak <ajayspakcommcom>
2025-02-20 15:22:00 +0530

    changes

--- src/controllers/moview/userController.js
+++ src/controllers/moview/userController.js
@@ -21,12 +21,61 @@ exports.getAllUsers = async(req, res) => {
     }
 };
 
+// exports.getOtherUserDetails = async (req, res) => {
+//     try {
+//         const { userId } = req.params;
+
+//         // Fetch user details
+//         const user = await User.findById(userId);
+//         if (!user) {
+//             return res.status(404).json({ status: 'error', message: 'User not found' });
+//         }
+
+//         // Count the number of followers
+//         const followersCount = await Follower.countDocuments({ userId: userId });
+
+//         // Count the number of users this user is following
+//         const followingCount = await Following.countDocuments({ userId: userId });
+
+//         // Find reviews by this user
+//         const reviews = await Review.find({ user: userId });
+//         const reviewShows = await ReviewShow.find({ user: userId });
+        
+//         // Separate reviews for movies and shows
+//         const movieReviews = reviews.filter(review => review.movie);
+//         const showReviews = reviewShows.filter(review => review.show);
+
+//         // Fetch the list of movies the user reviewed
+//         const movies = await Movie.find({ _id: { $in: movieReviews.map(r => r.movie) } });
+
+//         // Fetch the list of shows the user reviewed
+//         const shows = await Show.find({ _id: { $in: showReviews.map(r => r.show) } });
+
+//         // Send the response
+//         res.status(200).json({
+//                 status: 'success',
+//                 data: {
+//                     user,
+//                     totalReviewedMovies: movieReviews.length,
+//                     totalReviewedShows: showReviews.length,
+//                     followersCount,
+//                     followingCount,
+//                     reviewedMovies: movies,
+//                     reviewedShows: shows
+//                 }});
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ status: 'error', message: 'Server error: Cannot retrieve user details.' });
+//     }
+// };
+
 exports.getOtherUserDetails = async (req, res) => {
     try {
         const { userId } = req.params;
 
         // Fetch user details
         const user = await User.findById(userId);
+
         if (!user) {
             return res.status(404).json({ status: 'error', message: 'User not found' });
         }
@@ -37,27 +86,50 @@ exports.getOtherUserDetails = async (req, res) => {
         // Count the number of users this user is following
         const followingCount = await Following.countDocuments({ userId: userId });
 
-        // Find reviews by this user
-        const reviews = await Review.find({ user: userId });
-        const reviewShows = await ReviewShow.find({ user: userId });
-        
-        // Separate reviews for movies and shows
-        const movieReviews = reviews.filter(review => review.movie);
-        const showReviews = reviewShows.filter(review => review.show);
+        // Find reviews by this user with movies populated
+        const reviews = await Review.find({ user: userId }).populate('movie', 'title poster_url');
+        const reviewShows = await ReviewShow.find({ user: userId }).populate('show', 'title poster_url');
 
-        // Fetch the list of movies the user reviewed
-        const movies = await Movie.find({ _id: { $in: movieReviews.map(r => r.movie) } });
+        // Get correct counts
+        const totalReviewedMovies = reviews.length;
+        const totalReviewedShows = reviewShows.length;
 
-        // Fetch the list of shows the user reviewed
-        const shows = await Show.find({ _id: { $in: showReviews.map(r => r.show) } });
+        // Fetch the list of movies reviewed with ratings
+        const movies = reviews.map(review => ({
+            _id: review.movie ? review.movie._id : null,
+            title: review.movie ? review.movie.title : 'Unknown',
+            poster_url: review.movie ? review.movie.poster_url : '',
+            rating: review.rating
+        })).filter(movie => movie._id); // Remove null values
+
+        // Fetch the list of shows reviewed with ratings
+        const shows = reviewShows.map(review => ({
+            _id: review.show ? review.show._id : null,
+            title: review.show ? review.show.title : 'Unknown',
+            poster_url: review.show ? review.show.poster_url : '',
+            rating: review.rating
+        })).filter(show => show._id); // Remove null values
 
         // Send the response
-        res.status(200).json({status: 'success', data: {user,totalReviewedMovies: movieReviews.length,totalReviewedShows: showReviews.length,followersCount,followingCount,reviewedMovies: movies,reviewedShows: shows}});
+        res.status(200).json({
+            status: 'success',
+            data: {
+                user,
+                totalReviewedMovies,
+                totalReviewedShows,
+                followersCount,
+                followingCount,
+                reviewedMovies: movies,
+                reviewedShows: shows
+            }
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ status: 'error', message: 'Server error: Cannot retrieve user details.' });
     }
 };
+
+
 
 exports.getUserByName = async(req, res) => {
     try {
