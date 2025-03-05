5b66fe432da8b3323ab047f74529437a51ba6a56
Ajay Spak <ajayspakcommcom>
2025-03-05 12:29:29 +0530

    changes

--- src/models/moview/bannerModel.js
+++ src/models/moview/bannerModel.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bannerSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
     trending_banner: {
         type: String,
         required: true
